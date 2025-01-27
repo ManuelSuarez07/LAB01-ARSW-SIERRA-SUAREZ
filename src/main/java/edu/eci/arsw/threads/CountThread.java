1f9178fe92fba4cd1b907a0a526f4816c9241f32
Manuel Suarez <manuel.suarez.e09@gmail.com>
2025-01-27 18:50:16 -0500

    First Point Done

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -1,28 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package edu.eci.arsw.threads;
 
+package edu.eci.arsw.threads;
 /**
  *
- * @author hcadavid
+ * @author Sierra - Suarez
  */
-public class CountThread extends Threads{
-    private int A;
+class CountThread extends Thread {
+    private int A; 
     private int B;
 
-    /** Constructor de la clase CountThread*/ 
-    public CountThread(int A, int b){
+    public CountThread(int A, int B) {
         this.A = A;
         this.B = B;
     }
 
     @Override
-    public void run(){
-        for(int i = A, i <= B; i+){
-            System.out.println("numero : ")
+    public void run() {
+        for (int i = A; i <= B; i++) {
+            System.out.println(getName() + ": " + i);
         }
     }
 }
